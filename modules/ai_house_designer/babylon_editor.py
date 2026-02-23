4a8cc2101b05e6e9d02b5575ecca705c409d2996
ARCHIRAPID Developer <archirapid@project.local>
2026-02-23 09:25:59 +0100

    ✅ Fix paneles Babylon: actualización al escalar suelos, onDragEndObservable, variable js_scale_observer extraída del f-string, paneles verde y azul funcionan

--- modules/ai_house_designer/babylon_editor.py
+++ modules/ai_house_designer/babylon_editor.py
@@ -17,7 +17,35 @@ def generate_babylon_html(rooms_data, total_width, total_depth):
     
     import json
     rooms_js = json.dumps(rooms_data, ensure_ascii=False)
-    
+
+    js_scale_observer = """
+                    // Solo permitir escalar en X y Z (no en Y/altura)
+                    gizmoManager.gizmos.scaleGizmo.xGizmo.isEnabled = true;
+                    gizmoManager.gizmos.scaleGizmo.yGizmo.isEnabled = false;
+                    gizmoManager.gizmos.scaleGizmo.zGizmo.isEnabled = true;
+
+                    console.log('Modo: Escalar (arrastra cubos en esquinas)');
+
+                    gizmoManager.gizmos.scaleGizmo.onDragEndObservable.clear();
+
+                    if (selectedMesh.name.startsWith('floor_')) {
+                        const roomIndex = parseInt(selectedMesh.name.split('_')[1]);
+                        console.log('DEBUG: Añadiendo observable para roomIndex=' + roomIndex);
+                        gizmoManager.gizmos.scaleGizmo.onDragEndObservable.add(() => {
+                            console.log('DEBUG: Drag finalizado!');
+                            scene.registerAfterRender(function waitFrame() {
+                                scene.unregisterAfterRender(waitFrame);
+                                updateRoomInfo(selectedMesh, roomIndex);
+                                updateBudget();
+                                console.log('Paneles actualizados roomIndex=' + roomIndex);
+                            });
+                        });
+                    } else {
+                        alert('Primero selecciona una habitación con modo Seleccionar');
+                        setMode('select');
+                    }
+"""
+
     html = f"""
 <!DOCTYPE html>
 <html>
@@ -394,20 +422,7 @@ def generate_babylon_html(rooms_data, total_width, total_depth):
                     gizmoManager.positionGizmoEnabled = false;
                     gizmoManager.scaleGizmoEnabled = true;
                     
-                    // Solo permitir escalar en X y Z (no en Y/altura)
-                    gizmoManager.gizmos.scaleGizmo.xGizmo.isEnabled = true;
-                    gizmoManager.gizmos.scaleGizmo.yGizmo.isEnabled = false;  // Sin escalar altura
-                    gizmoManager.gizmos.scaleGizmo.zGizmo.isEnabled = true;
-                    
-                    console.log('Modo: Escalar (arrastra cubos en esquinas)');
-                    
-                    // Actualizar info al escalar
-                    if (selectedMesh.name.startsWith('floor_')) {{
-                        const roomIndex = parseInt(selectedMesh.name.split('_')[1]);
-                        selectedMesh.onAfterWorldMatrixUpdateObservable.add(() => {{
-                            updateRoomInfo(selectedMesh, roomIndex);
-                        }});
-                    }}
+                    {js_scale_observer}
                 }} else {{
                     alert('Primero selecciona una habitación con modo Seleccionar');
                     setMode('select');
